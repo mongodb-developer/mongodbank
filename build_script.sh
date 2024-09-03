3072be06ee4bde230c4c4f6b1794988758fb63c9
Michael Lynn <merlynn@gmail.com>
2024-09-03 05:17:52 -0400

    modify build to support psychopg2

--- build_script.sh
+++ build_script.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-pip install --upgrade pip
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Update pip
+python -m pip install --upgrade pip
+
+# Install wheel
 pip install wheel
+
+# Install the requirements
 pip install -r requirements.txt
